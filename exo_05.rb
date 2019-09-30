2573dbbb340f7f31e3b85e1b609b726b6045f115
Guillaume954 <guillaume.farina95@gmail.com>
2019-09-30 18:31:39 +0200

    exo 5

---
+++ exo_05.rb
@@ -0,0 +1,60 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"  
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" 
+=begin 
+le # permet de calculer les valeurs entre crochet 
+=end
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 
+=begin 
+c'est une variable, elle est calculer automatiquement
+=end
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+=begin
+C'est une string, ce qui est compris entre guillements s'affiche en dur
+=end 
+
+puts 3 + 2 < 5 - 7
+=begin
+c'est un booléen "-2" étant plus petit que 5, la réponse renvoyé est considéré comme fausse.
+=end
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+=begin
+il y a une string donc le tout est affiché. Le résultat entre crochet et concatainer dans la string
+=end 
+
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+=begin
+Même réponse que celle du dessus
+=end 
+
+puts "Ok, c'est faux alors !"
+=begin
+c'est une string
+=end
+
+puts "C'est drôle ça, faisons-en plus :"
+=begin
+c'est une string
+=end 
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+=begin
+Booléen qui renvois les réponses false ou true en fonction de la valeur entre crochet. Ce sont les opérateurs  (<= et =< et >) qui font que c'est un booléen
+
+=end
+
+
+
+
+
+
+
+
+
