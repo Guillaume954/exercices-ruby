4a63eb57fb74513ea91725df66fba3f607b9baba
remi <rr03ml@gmail.com>
2019-10-01 11:58:15 +0200

    exo16

--- exo_16.rb
+++ exo_16.rb
@@ -1,10 +1,10 @@
 puts "Quel est ton année de naissance ?"
 date =gets.chomp
-age = 0
+y = 0
 x = 2019 - date.to_i
 until date ==2019
 date = date.to_i + 1
-age = age.to_i + 1
+y = y.to_i + 1
 x = x.to_i - 1
-puts "Il y a #{x} ans, tu avais #{age} ans"
+puts "Il y a #{x} ans, tu avais #{y} ans"
 end
